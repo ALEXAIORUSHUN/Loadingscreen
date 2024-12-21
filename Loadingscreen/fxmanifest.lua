8c6d5dd7c55a719941f3e22befb5b48348d17a5e
ALEXAIORUSHUN <fsccasebox@gmail.com>
2024-12-21 21:32:04 +0100

    Loadingscreen
    Betöltőképernyő

---
+++ Loadingscreen/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Anonymus'
+description 'Loadingscreen'
+version '1.0.0'
+
+files {
+    'index.html',
+    'style.css',
+    'background.jpg',
+    'music/music.mp3',
+    'images/*.png'
+}
+
+loadscreen 'index.html'
